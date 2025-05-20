debd8ef0e2d94b12df36ca8564b5257d3a93d1bf
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-20 10:02:21 +0700

    class stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -7,4 +7,26 @@ class Node
 public:
     int data;
     Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+class Stack
+{
+ private :
+    Node *top;
+
+public:
+    Stack()
+    {
+        top = NULL;
+    }
+
+    int push(int value)
+    {
+        
+    }
 }
